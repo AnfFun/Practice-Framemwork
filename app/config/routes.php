cf51b153639c59db22ef8cecae9e9b941d2f7150
AnfFun <prokofmaxwork@gmail.com>
2023-02-11 22:22:19 +0200

    Add testing routes

---
+++ app/config/routes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'genre' => [
+        'controller' => 'genre',
+        'action' => 'show'
+    ],
+    'visitors' => [
+        'controller' => 'visitors',
+        'action' => 'show'
+    ],
+
+];
